3ef0979d7b5caa05e5033118fc243d534872694a
ArturoTrelles <trelles91n.n@gmail.com>
2025-02-04 12:55:29 -0600

    Primera prueba

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
 
+////alert ("Hay que empezar el proyecto ");
 
-let amigos = []
-alert ("Hay que empezar el proyecto ");
-prompt("Necesito un nombre:");
+// Lista de amigos como un array - Se inicializa con = []
+
+let AmigoSecreto = prompt("Necesito un nombre:"); 
+
+//let amigos = []
+let amigos = ["Arturo"]
+
+//alert ("Hay que empezar el proyecto ");
+
+if (AmigoSecreto == amigos)
+    alert('Acertaste el Amigo Secreto')
+else
+    alert('No acertaste')
+//console.log(nombreDeUsuario)
+
