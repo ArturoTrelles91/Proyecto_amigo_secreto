27fd616a80a2b610397dc11da3d77daa30ea489c
ArturoTrelles <trelles91n.n@gmail.com>
2025-02-04 13:09:51 -0600

    Prueba con template

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-////alert ("Hay que empezar el proyecto ");
-
 // Lista de amigos como un array - Se inicializa con = []
 
 let AmigoSecreto = prompt("Necesito un nombre:"); 
@@ -10,11 +8,13 @@ let AmigoSecreto = prompt("Necesito un nombre:");
 //let amigos = []
 let amigos = ["Arturo"]
 
-//alert ("Hay que empezar el proyecto ");
 
-if (AmigoSecreto == amigos)
-    alert('Acertaste el Amigo Secreto')
-else
-    alert('No acertaste')
+if (AmigoSecreto == amigos){
+    // La condicion se cumple
+    alert(`Acertaste, el Amigo Secreto es: ${amigos}`);
+    } else {
+        // La condicion no se cumple
+    alert('No acertaste');
+}
 //console.log(nombreDeUsuario)
 
