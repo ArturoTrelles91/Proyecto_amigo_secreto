24c0ac36c610acce48f295209992cc2a6b370dbd
ArturoTrelles <trelles91n.n@gmail.com>
2025-02-04 15:19:37 -0600

    Proximo Objetivo

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Vamos a definir un numero aleatorio que despues vamos a usar para buscar lugares en un arreglo
+// Debemos llenar amigos con varios nombres, luego hay que calcular el tamaño del arreglo y usar ese tamaño para generar numeros aleatorios en el rango de 1 a lenght_array
+
+//parseint ?
+
+let amigos = []
+larreglo  = length(amigos)
+let numeroEnArreglo = Math.floor(Math.random()*larreglo)+1;
+let AmigoSecreto = amigos[numeroEnArreglo]
+
+
+let intentos = 0;
+
+
 // Lista de amigos como un array - Se inicializa con = []
-
-
-let intentos = 1;
-
-// let AmigoSecreto = prompt("Necesito un nombre para empezar:"); 
-
 // let amigos = []
 let amigos = ["Arturo"]
-
+console.log(typeof(amigos))
 let AmigoSecreto = prompt("Necesito un nombre para empezar:"); 
 
 if(AmigoSecreto == amigos){
    alert('Acertaste');
+   intentos = intentos+1;
   }
    else{
     alert('No acertaste');
+    intentos = intentos+1
    }
 
 while(AmigoSecreto != amigos){
@@ -29,6 +39,12 @@ while(AmigoSecreto != amigos){
         } else {
             // La condicion no se cumple
         alert(`Acertaste, el Amigo Secreto es: ${amigos} lo hiciste en ${intentos}`);
+        break
+    }
+
+    if(intentos >= 5){
+        alert('Llegaste al numero maximo de intentos');
+        break
     }
    
 }    
