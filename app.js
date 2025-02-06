0151f75b65bf44d184c1c78375f6913795881d03
ArturoTrelles <trelles91n.n@gmail.com>
2025-02-06 15:39:33 -0600

    Version Final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
-
+// Programa Final
 let amigos = []
 
 
